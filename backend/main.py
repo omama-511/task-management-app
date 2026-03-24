dcb3567f16707c7c003d8e68c2e386a3fff23812
Omama Arshad <omamaarshad1@gmail.com>
2026-03-24 14:32:23 +0000

    Update main.py
    Added url of frontend deployed on vercel

--- backend/main.py
+++ backend/main.py
@@ -22,8 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
-      # Angular dev server
+    allow_origins=["http://localhost:4200"],['http://task-management-app-po2p.vercel.app/'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
